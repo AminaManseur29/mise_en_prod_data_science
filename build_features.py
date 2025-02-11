6a2a49287a714f5e8bcde9c8ff7969d5ab92acdf
amanseur <amina.manseur@ensae.fr>
2025-02-11 09:57:55 +0000

    Modularisation

--- build_features.py
+++ build_features.py
@@ -1,11 +1,3 @@
-"""
-Prediction de la survie d'un individu sur le Titanic
-"""
-
-import os
-from dotenv import load_dotenv
-import argparse
-
 import pandas as pd
 
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
@@ -14,49 +6,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import confusion_matrix
-
-
-# ENVIRONMENT CONFIGURATION ---------------------------
-
-load_dotenv()
-
-parser = argparse.ArgumentParser(description="Paramètres du random forest")
-parser.add_argument(
-    "--n_trees", type=int, default=20, help="Nombre d'arbres"
-)
-args = parser.parse_args()
-
-n_trees = args.n_trees
-jeton_api = os.environ.get("JETON_API", "")
-data_path = os.environ.get("DATA_PATH", "data.csv")
-MAX_DEPTH = None
-MAX_FEATURES = "sqrt"
-
-if jeton_api.startswith("$"):
-    print("API token has been configured properly")
-else:
-    print("API token has not been configured")
-
-
-# FUNCTIONS --------------------------
-
-
-def split_and_count(df, column, separator):
-    """
-    Split a column in a DataFrame by a separator and count the number of resulting elements.
-
-    Args:
-        df (pandas.DataFrame): The DataFrame containing the column to split.
-        column (str): The name of the column to split.
-        separator (str): The separator to use for splitting.
-
-    Returns:
-        pandas.Series: A Series containing the count of elements after splitting.
-
-    """
-    return df[column].str.split(separator).str.len()
-
 
 def split_train_test(data, test_size, train_path="train.csv", test_path="test.csv"):
     """
@@ -154,60 +103,3 @@ def create_pipeline(
     )
 
     return pipe
-
-
-def evaluate_model(pipe, X_test, y_test):
-    """
-    Evaluate the model by calculating the score and confusion matrix.
-
-    Args:
-        pipe (sklearn.pipeline.Pipeline): The trained pipeline object.
-        X_test (pandas.DataFrame): The test data.
-        y_test (pandas.Series): The true labels for the test data.
-
-    Returns:
-        tuple: A tuple containing the score and confusion matrix.
-    """
-    score = pipe.score(X_test, y_test)
-    matrix = confusion_matrix(y_test, pipe.predict(X_test))
-    return score, matrix
-
-
-
-
-
-# IMPORT ET EXPLORATION DONNEES --------------------------------
-
-TrainingData = pd.read_csv("data.csv")
-
-
-# Usage example:
-ticket_count = split_and_count(TrainingData, "Ticket", "/")
-name_count = split_and_count(TrainingData, "Name", ",")
-
-
-# SPLIT TRAIN/TEST --------------------------------
-
-X_train, X_test, y_train, y_test = split_train_test(TrainingData, test_size=0.1)
-
-
-# PIPELINE ----------------------------
-
-
-# Create the pipeline
-pipe = create_pipeline(
-    n_trees, max_depth=MAX_DEPTH, max_features=MAX_FEATURES
-)
-
-
-# ESTIMATION ET EVALUATION ----------------------
-
-pipe.fit(X_train, y_train)
-
-
-# Evaluate the model
-score, matrix = evaluate_model(pipe, X_test, y_test)
-print(f"{score:.1%} de bonnes réponses sur les données de test pour validation")
-print(20 * "-")
-print("matrice de confusion")
-print(matrix)
