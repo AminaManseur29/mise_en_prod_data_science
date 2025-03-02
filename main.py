dcdc46006bad2ec7015fb7fb19bec1b1a8591c7a
amanseur <amina.manseur@ensae.fr>
2025-03-02 09:47:21 +0000

    stocker les données de manière externe

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ Prediction de la survie d'un individu sur le Titanic
 """
 
 import os
+import pathlib
 from dotenv import load_dotenv
+from loguru import logger
 import argparse
 import pandas as pd
-from loguru import logger
+from sklearn.model_selection import train_test_split
 
-from src.data.import_data import split_and_count
 from src.pipeline.build_features import split_train_test, create_pipeline
 from src.models.train_evaluate import evaluate_model
 
@@ -24,9 +25,13 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
+URL_RAW = "https://minio.lab.sspcloud.fr/amanseur/ensae-reproductibilite/data/raw/data.csv"
+
 n_trees = args.n_trees
 jeton_api = os.environ.get("JETON_API", "")
-data_path = os.environ.get("DATA_PATH", "data/row/data.csv")
+data_path = os.environ.get("data_path", URL_RAW)
+data_train_path = os.environ.get("train_path", "data/derived/train.parquet")
+data_test_path = os.environ.get("test_path", "data/derived/test.parquet")
 MAX_DEPTH = None
 MAX_FEATURES = "sqrt"
 
@@ -38,17 +43,21 @@ else:
     # print("API token has not been configured")
 
 
-# FUNCTIONS --------------------------
-
-
 # IMPORT ET EXPLORATION DONNEES --------------------------------
+
+p = pathlib.Path("data/derived/")
+p.mkdir(parents=True, exist_ok=True)
 
 TrainingData = pd.read_csv(data_path)
 
+y = TrainingData["Survived"]
+X = TrainingData.drop("Survived", axis="columns")
 
-# Usage example:
-ticket_count = split_and_count(TrainingData, "Ticket", "/")
-name_count = split_and_count(TrainingData, "Name", ",")
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.1
+)
+pd.concat([X_train, y_train], axis=1).to_parquet(data_train_path)
+pd.concat([X_test, y_test], axis=1).to_parquet(data_test_path)
 
 
 # SPLIT TRAIN/TEST --------------------------------
